14f28c05d53531992d09bb45f1788e98e44c596c
Christoph Heindl <christoph.heindl@gmail.com>
2014-03-05 15:13:35 +0100

    Fiddling with parameters, still not working

--- test/nlls_gaussnewton_alignpoints.cpp
+++ test/nlls_gaussnewton_alignpoints.cpp
@@ -21,7 +21,10 @@ int main() {
     
     // The scene points will be a translated and rotated version of the model points plus some optional noise.
     typedef Eigen::Transform<cppopt::Scalar, 2, Eigen::Affine> AffineTransform2D;
-    AffineTransform2D torig = Eigen::Translation2f(20.f, -10.f) * Eigen::Rotation2Df(0.4f);
+    //AffineTransform2D torig = Eigen::Translation2f(2.f, 2.f) * Eigen::Rotation2Df(0.0f);
+    AffineTransform2D torig;
+    torig.setIdentity();
+    torig.translate(Eigen::Vector2f(2.f, 2.f));
 
     cppopt::Matrix sp(20, 2);
     for (int i = 0; i < mp.rows(); ++i) {
@@ -32,14 +35,16 @@ int main() {
     cppopt::F f = [&mp, &sp](const cppopt::Matrix &x) -> cppopt::Matrix {
         cppopt::Matrix y(mp.rows(), 1);
 
-        AffineTransform2D t = Eigen::Translation2f(x(1), x(2)) * Eigen::Rotation2Df(x(0));
-        
+        AffineTransform2D t;
+        t.setIdentity();
+        t.translate(Eigen::Vector2f(x(0), x(1)));//Eigen::Translation2f(x(0), x(1))); //* Eigen::Rotation2Df(0);
+
         for (int i = 0; i < y.rows(); ++i) {
             Eigen::Vector2f m = mp.row(i);
             Eigen::Vector2f s = sp.row(i);
-            y(i) = (m - t * s).norm();
+            y(i) = 0 - (m - t * s).norm();
         }
-        
+       
         return y;
     };
     
@@ -47,8 +52,13 @@ int main() {
     cppopt::F df = [&mp, &sp](const cppopt::Matrix &x) -> cppopt::Matrix {
         cppopt::Matrix d(mp.rows(), x.rows());
 
-        AffineTransform2D t = Eigen::Translation2f(x(1), x(2)) * Eigen::Rotation2Df(x(0));
+       
+        //AffineTransform2D t = Eigen::Translation2f(x(0), x(1)) * Eigen::Rotation2Df(0);
+        AffineTransform2D t;
+        t.setIdentity();
+        t.translate(Eigen::Vector2f(x(0), x(1)));
         
+         /*
         AffineTransform2D td_dth;
         td_dth.matrix() << -sinf(x(0)), -cosf(x(0)), 0
                          , cosf(x(0)), -sinf(x(0)), 0
@@ -63,42 +73,54 @@ int main() {
         td_dty.matrix() << 0, 0, 0
                          , 0, 0, 1
                          , 0, 0, 1;
-        
+        */
+
         for (int i = 0; i < d.rows(); ++i) {
             Eigen::Vector2f m = mp.row(i);
             Eigen::Vector2f s = sp.row(i);
-            Eigen::Vector2f st = t * s;
-            Eigen::Vector2f sdt_dth = td_dth * st;
-            Eigen::Vector2f sdt_dtx = td_dtx * st;
-            Eigen::Vector2f sdt_dty = td_dty * st;
+            //Eigen::Vector2f st = t * s;
+            //Eigen::Vector2f sdt_dth = td_dth * st;
+            //Eigen::Vector2f sdt_dtx = td_dtx * st;
+            //Eigen::Vector2f sdt_dty = td_dty * st;
 
-            cppopt::Scalar denom = 2.0f * (m - st).norm();
-            d(i, 0) = (-2.f * (m(0) - st(0)) * sdt_dth(0) - 2.f * (m(1) - st(1)) * sdt_dth(1)) / denom;
-            d(i, 1) = (-2.f * (m(0) - st(0)) * sdt_dtx(0) - 2.f * (m(1) - st(1)) * sdt_dtx(1)) / denom; 
-            d(i, 2) = (-2.f * (m(0) - st(0)) * sdt_dty(0) - 2.f * (m(1) - st(1)) * sdt_dty(1)) / denom;
-        }
-        
+            cppopt::Scalar denom = (m - t * s).norm();
+            
+            // df/dtheta f = sqrt(|m - R(theta)s|)
+            
+            /*
+            d(i, 0) = (2.f * (m(0) - s(0)*cosf(x(0)) + s(1)*sinf(x(0))) * (s(0)*sinf(x(0)) + s(1)*cosf(x(0)))) +
+                      (2.f * (m(1) - s(0)*sinf(x(0)) - s(1)*cosf(x(0))) * (-s(0)*cosf(x(0)) + s(1)*sinf(x(0))));
+            */
+            //d(i,0) = 0;
+            d(i, 0) = (m(0) - s(0) - x(0)) / denom; 
+            d(i, 1) = (m(1) - s(1) - x(1)) / denom; 
+            
+        }        
         
         return d;
     };
 
     // Create start solution
-    cppopt::Matrix x(3, 1);
-    x << 0.0f, 0.f, 0.0f;
+    cppopt::Matrix x(2, 1);
+    x << 0.0f, 0.f;
     
     // Iterate while norm of residual is greater than a user-selected threshold.
-    while (f(x).norm() > 0.001f) {
+    int i = 0;
+    while ( i < 3) {
         cppopt::gaussNewton(f, df, x);
         std::cout
             << std::fixed << std::setw(3)
             << "Parameters: " << x.transpose()
             << " Error: " << f(x).norm() << std::endl;
+        ++i;
     }
 
+    /*
     AffineTransform2D tfinal = Eigen::Translation2f(x(1), x(2)) * Eigen::Rotation2Df(x(0));
     // We are actually searching for the inverse of torig i.e the matrix that brings back the 
     // transformed scenepoints into alignment with the model points.
     std::cout << (torig * tfinal).matrix() << std::endl;
+    */
     return 0;
     
 }

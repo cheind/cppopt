98ca09a47c630e642e214968826156736bd9b99d
Christoph Heindl <christoph.heindl@gmail.com>
2014-03-09 18:19:33 +0100

    Documentation updates.

--- test/1d_newtonraphson.cpp
+++ test/1d_newtonraphson.cpp
@@ -11,18 +11,30 @@
 #include <iostream>
 #include <iomanip>
 
-/*
- This example optimizes f(x) = 3x^3 - 10x^2 - 56x + 5 with start value x = 2.
- 
- Note that
-    f'(x)  = 9x^2 - 20x - 56
-    f''(x) = 18x - 20
+/** This example finds a local extremum of a third order scalar valued polynomial using the Newton-Raphson algorithm.
+ *
+ *  The function to be optimized is given by 
+ *
+ *      f(x) = 3x^3 - 10x^2 - 56x + 5
+ *
+ *  In order to use Newton-Raphson for optimization the first and second order derivates are
+ *  required, which are given by
+ *
+ *      df/dx   = 9x^2 - 20x - 56
+ *      df/dxdx = 18x - 20
+ *
+ *  Note that the Newton-Raphson method is usually used for root finding where one only requires the function and
+ *  the first order derivates. Since stationary points (extremum points, saddle points) are defined by having a gradient
+ *  of zero (i.e root finding of the first order derivative)
+ *
+ *      df/dx = 0
+ *
+ *  we will simply pass the first and second order derivative into Newton-Raphson.
  */
- 
-
 
 int main() {
 
+    // Define the first order derivative
     cppopt::F df = [](const cppopt::Matrix &x) -> cppopt::Matrix {
         cppopt::Matrix d(1, 1);
         
@@ -31,6 +43,7 @@ int main() {
         return d;
     };
     
+    // Define the second order derivative
     cppopt::F ddf = [](const cppopt::Matrix &x) -> cppopt::Matrix {
         cppopt::Matrix d(1, 1);
         
@@ -39,11 +52,15 @@ int main() {
         return d;
     };
     
+    // Create a start solution. Note that this method does not necessarily find a minimum. Depending on its start value,
+    // it will convert to a minimum, maximum or saddle point. For example, try to use zero as start solution and the algorithm
+    // will find a maximum instead of a minimum.
     cppopt::Matrix x(1, 1);
-    x(0) = 2.f; // Try to start with 0 and you fill find a maximum.
+    x(0) = 2.f;
     
-    // Iterate while norm of residual is greater than a user-selected threshold.
-    while (df(x).norm() > 0.001f) {
+    // Iterate while norm of the first order derivative is greater than some predefined threshold.
+    cppopt::ResultInfo ri = cppopt::SUCCESS;
+    while (ri == cppopt::SUCCESS && df(x).norm() > 0.001f) {
         cppopt::newtonRaphson(df, ddf, x);
         std::cout
             << std::fixed << std::setw(3)

ee664e2f66bee9a9bbc7b36873e8ab527d5f7a23
Christoph Heindl <christoph.heindl@gmail.com>
2014-03-11 10:47:35 +0100

    Refactoring of numerical derivatives
    The second order derivatives are still not working, which is why they
    are commented out. I suppose this is to numerical rounding and
    truncation issues.

--- inc/cppopt/finite_difference.h
+++ inc/cppopt/finite_difference.h
@@ -37,20 +37,45 @@ namespace cppopt {
             // Note this dance is only necessary due to numerical rounding issues.
             // See http://en.wikipedia.org/wiki/Numerical_differentiation for details.
             
-            //const T eps = static_cast<T>(sqrt(std::numeric_limits<T>::epsilon()));
-            /*const T h = 0.00001f;
-            //const T h = eps * x;
+            const T eps = static_cast<T>(sqrt(std::numeric_limits<T>::epsilon()));
+            const T h = eps * x;
             volatile T xph = x + h;
-            return xph - x;*/
+            return xph - x;   
+        }
 
-            return 0.001f;
-            
-        }  
+        /** Utility class to evaluate functions at differences specified by offsets */
+        class FiniteDifferenceHelper {
+        public:
+            FiniteDifferenceHelper(const F &f, const Dims &dims)
+                : _f(f), _offset(dims.x_rows, 1)
+            {}
+
+            Matrix operator()(const Matrix &x, int dim, Scalar h) {
+                _offset.setZero();
+                _offset(dim) = h;
+                return _f(x + _offset);
+            }
+
+            Matrix operator()(const Matrix &x, int dim0, Scalar h0, int dim1, Scalar h1) {
+                _offset.setZero();
+                _offset(dim0) = h0;
+                _offset(dim1) = h1;
+                return _f(x + _offset);
+            }
+
+            Matrix operator()(const Matrix &x) {
+                return _f(x);
+            }
+
+        private:
+            const F &_f;
+            Matrix _offset;
+        };
     }
     
     /** Approximates the nth order derivative for a multivariate scalar valued function using forward difference. */
     template<int Order>
-    class ApproximateForwardDerivative {};
+    class ApproximateCentralDerivative {};
 
     /** Numerical approximation of the first order partial derivatives. 
       *
@@ -58,11 +83,11 @@ namespace cppopt {
       * univariate or multivariate.
       */
     template<>
-    class ApproximateForwardDerivative<1> {
+    class ApproximateCentralDerivative<1> {
     public:
         
         /** Initialize */
-        ApproximateForwardDerivative(const F & f, const Dims &dims) 
+        ApproximateCentralDerivative(const F & f, const Dims &dims) 
             : _f(f), _dims(dims)
         {}
 
@@ -70,14 +95,11 @@ namespace cppopt {
         Matrix operator()(const Matrix &x) const
         {
             Matrix d(_dims.y_rows, _dims.x_rows);
-            Matrix offset(_dims.x_rows, 1);
+            internal::FiniteDifferenceHelper fdh(_f, _dims);
 
             for (int i = 0; i < x.rows(); ++i) {
-                offset.setZero();
-            
-                const Scalar dx = internal::findSuitableH(x(i));
-                offset(i, 0) = dx;               
-                d.col(i) = (_f(x + offset) - _f(x)) / dx;
+                const Scalar dx = internal::findSuitableH(x(i));             
+                d.col(i) = (fdh(x, i, dx) - fdh(x, i, -dx)) / (Scalar(2) * dx);
             }
 
             // By definition of cppopt gradient vectors are column vectors, whereas the 
@@ -96,41 +118,36 @@ namespace cppopt {
 
     /** Numerical approximation of the second order partial derivatives. 
       *
-      * This method works for real valued functions that are either
+      * This method works for real and vector valued functions that are either
       * univariate or multivariate.
-      *
-      * This method repeatedly calls ApproximateForwardDerivative<1> using first
-      * order derivative approximation of the input function.
     template<>
-    class ApproximateForwardDerivative<2> {
+    class ApproximateCentralDerivative<2> {
     public:
         
-        ApproximateForwardDerivative(const F & f, const Dims &dims) 
-            : _f(f), _dims(dims)
-        {
-        }
+        ApproximateCentralDerivative(const F & f, const Dims &dims) 
+            : _f(f), _dims(dims), _offset(dims.x_rows, 1)
+        {}
 
         Matrix operator()(const Matrix &x) const
         {
-            const ApproximateForwardDerivative<1> df(_f, _dims);
-
             Matrix d(_dims.x_rows, _dims.x_rows);
-            Matrix offset(_dims.x_rows, 1);
+            internal::FiniteDifferenceHelper fdh(_f, _dims);
 
-            for (int i = 0; i < x.rows(); ++i) {
-                offset.setZero();
-            
-                const Scalar dx = internal::findSuitableH(x(i));
-                offset(i, 0) = dx;
-                d.col(i) = (df(x + offset) - df(x)) / dx;
-            }
-
-            // By definition of cppopt gradient vectors are column vectors, whereas the 
-            // Jacobian is defined by having the partial derivates in columns.
-            if (d.rows() == 1) {
-                return d.transpose();
-            } else {
-                return d;
+            for (int r = 0; r < x.rows(); ++r) {
+                for (int c = 0; c < x.rows(); ++c) {
+                    const Scalar dr = internal::findSuitableH(x(r));                    
+                    if (c == r) {
+                        d(r, c) = (fdh(x, r, dr) - 
+                                   Scalar(2) * fdh(x) + 
+                                   fdh(x, r, -dr))(0) / (dr * dr);
+                    } else {
+                        const Scalar dc = internal::findSuitableH(x(c));
+                        d(r, c) = (fdh(x, r, dr, c, dc) - 
+                                   fdh(x, r, dr, c, -dc) -
+                                   fdh(x, r, -dr, c, dc) +
+                                   fdh(x, r, -dr, c, -dc))(0) / (Scalar(4) * dr * dc);
+                    }
+                }
             }
 
             return d;
@@ -139,8 +156,9 @@ namespace cppopt {
     private:
         F _f;
         Dims _dims;
+        Matrix _offset;
     };
-          */
+    */
 }
 
 #endif

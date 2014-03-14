6823afc21ee0339b370c5bdf50bd24c830656d0e
Christoph Heindl <christoph.heindl@gmail.com>
2014-03-14 18:40:24 +0100

    Added empty tests project.

---
+++ tests/main.cpp
@@ -0,0 +1,13 @@
+// This file is part of cppopt, a lightweight C++ library
+// for numerical optimization
+//
+// Copyright (C) 2014 Christoph Heindl <christoph.heindl@gmail.com>
+//
+// This Source Code Form is subject to the terms of the BSD 3 license.
+// If a copy of the MPL was not distributed with this file, You can obtain
+// one at http://opensource.org/licenses/BSD-3-Clause.
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+/** cppopt uses CATCH to perform unit testing. 'catch.hpp' is a copy of https://github.com/philsquared/Catch/tree/master/single_include */
---
+++ tests/test_numerical_derivatives.cpp
@@ -0,0 +1,16 @@
+// This file is part of cppopt, a lightweight C++ library
+// for numerical optimization
+//
+// Copyright (C) 2014 Christoph Heindl <christoph.heindl@gmail.com>
+//
+// This Source Code Form is subject to the terms of the BSD 3 license.
+// If a copy of the MPL was not distributed with this file, You can obtain
+// one at http://opensource.org/licenses/BSD-3-Clause.
+
+#include "catch.hpp"
+#include <cppopt/numerical_derivative.h>
+
+namespace co = cppopt;
+
+TEST_CASE("First order numerical derivative") {
+}
